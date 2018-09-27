ec444f39c2ccba6f39f6b969e23f925f839da63c
Mocson <kimura.ryota.as@gmail.com>
2018-09-27 10:21:23 +0900

    Cut追加

--- KeyframeBox.py
+++ KeyframeBox.py
@@ -4,6 +4,7 @@ import maya.mel as mel
 # timeSliderCopyKey;
 # timeSliderPasteKey false;
 # timeSliderClearKey;
+# timeSliderCutKey;
 
 def cpy():
     mel.eval("timeSliderCopyKey;")
@@ -14,12 +15,17 @@ def pst():
 def dlt():
     mel.eval("timeSliderClearKey;")
 
+def cut():
+    mel.eval("timeSliderCutKey;")
+
 cmds.window(title='KeyframeBox', w=200)
 cmds.columnLayout(adj=True)
 cmds.separator( h=3 )
 cmds.text("KeyframeBox")
 cmds.separator( h=3 )
 cmds.button( label = 'Copy', w=100, h=40, command='cpy()')
+
+cmds.button( label = 'Cut', w=100, h=40, command='cut()')
 
 cmds.button( label = 'Paste', w=100, h=40, command='pst()')
 
